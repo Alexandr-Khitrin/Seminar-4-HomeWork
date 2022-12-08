1cb7916221b7869991e1ebf784a93612272f9e9d
Alexandr <hitrin79@gmail.com>
2022-12-09 00:34:01 +0800

    Zadacha 25

---
+++ HomeWork Seminar-4 Zadacha 25/HomeWork Seminar-4 Zadacha 25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите значение A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите значение B: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Degree(a, b));
+int Degree(int a, int b)
+{
+    int degree = 1;
+    int i = 0;
+    while (i < b)
+    {
+        degree *= a;
+        i++;
+    }
+    return degree;
+}
